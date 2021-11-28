9bbc213f75f608dc26b38458c4040753a2a9c6cb
Julian <julianbaier@gmail.com>
2021-11-28 23:20:11 +0100

    Continuing trying out GPy

--- main.py
+++ main.py
@@ -12,6 +12,3 @@ RBF_kernel = GPy.kern.RBF(input_dim=2)
 plt.show()
 model = GPy.models.gp_regression.GPRegression(X, Y, RBF_kernel)
 
-X_ = np.column_stack((np.linspace(0, 1, 59), sd.jagged_data(59, dim=1)))
-
-Y_ = model.predict_noiseless(X_)
