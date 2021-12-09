c0e157ab7a782da8ce0e6beb1e6c4af96102ba3a
julian-ba <julianbaier2001@gmail.com>
2021-12-09 17:23:18 +0100

    Improved plotting

--- gpr_functions.py
+++ gpr_functions.py
@@ -3,11 +3,11 @@ import tensorflow_probability as tfp
 import gpflow as gpf
 
 
-def simple_rbf_regression(x, fx, variance=1., lengthscales=1.):
+def simple_rbf_regression(x, fx, variance=1., lengthscales=1):
     rbf_model = gpf.models.GPR(
         data=(x, fx),
         kernel=gpf.kernels.stationaries.SquaredExponential(variance=variance, lengthscales=lengthscales),
     )
-    rbf_model.likelihood.variance.assign(0.2 * max(abs(fx[0])))
+    rbf_model.likelihood.variance.assign(0.02)
     return rbf_model
 
