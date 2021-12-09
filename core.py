e7ec6c8ad5ee01963b876eeea8a34a548cf3a424
julian-ba <julianbaier2001@gmail.com>
2021-12-09 14:56:46 +0100

    Created plotting - added two plotting functions to plotting.py - added automatic simple rbf regression

--- core.py
+++ core.py
@@ -1,3 +1,15 @@
 # TODO: Create function which returns a list of the coordinates of a grid.
 import numpy as np
 
+
+def atleast_column(x):
+    try:
+        x = np.asarray(x)
+        if x.ndim == 1:
+            output_array = x[:, np.newaxis]
+        else:
+            output_array = x
+    except TypeError:
+        raise TypeError("The input must be a NumPy-array.")
+
+    return output_array
