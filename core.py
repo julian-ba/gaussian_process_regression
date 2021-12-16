9d8b13d35172697af3a24acfa2f03f600f40ffeb
julian-ba <julianbaier2001@gmail.com>
2021-12-16 18:49:17 +0100

    Added a 2d-model plot function which could be made prettier

--- core.py
+++ core.py
@@ -5,6 +5,9 @@ import numbers
 
 
 def rbf_regression(x, fx, variance=1., lengthscales=1, noise_value=None):
+    x = atleast_column(x=x)
+    fx = atleast_column(x=fx)
+
     if noise_value is None:
         noise_value = max(np.abs(fx))[0] * 0.2
     rbf_model = gpf.models.GPR(
@@ -12,10 +15,12 @@ def rbf_regression(x, fx, variance=1., lengthscales=1, noise_value=None):
         kernel=gpf.kernels.stationaries.SquaredExponential(variance=variance, lengthscales=lengthscales),
     )
     rbf_model.likelihood.variance.assign(noise_value)
+
     return rbf_model
 
 
 def atleast_column(x):
+
     try:
         x = np.asarray(x)
         if x.ndim == 1:
@@ -28,19 +33,15 @@ def atleast_column(x):
     return output_array
 
 
-def coordinate_list(lower, upper, dim, n_per_dimension):
-    if isinstance(lower, numbers.Number):
-        lower = [lower for i in range(dim)]
+# def flatten_values(x, output_dim=1):
 
-    if isinstance(upper, numbers.Number):
-        upper = [upper for i in range(dim)]
 
-    if isinstance(n_per_dimension, numbers.Number):
-        n_per_dimension = [n_per_dimension for i in range(dim)]
+def coord_list(*xi):
+    coord_grid = np.reshape(np.stack(np.meshgrid(*xi), axis=-1), (-1, len(xi)))
+    return coord_grid
 
-    list_of_axes = []
-    for i in range(dim):
-        list_of_axes.append(np.linspace(lower[0], upper[0], n_per_dimension[0]))
 
-    coord_grids = np.meshgrid(*list_of_axes)
-
+def coord_list_and_meshgrid(*xi):
+    meshxi = np.meshgrid(*xi)
+    coord_grid = np.reshape(np.stack(meshxi, axis=-1), (-1, len(xi)))
+    return coord_grid, meshxi
