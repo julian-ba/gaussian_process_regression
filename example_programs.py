21523dd8be3c243c24173e5ff40c21d46baa6ec0
julian-ba <julianbaier2001@gmail.com>
2022-01-07 11:42:11 +0100

    added output.tif to repo

--- example_programs.py
+++ example_programs.py
@@ -16,24 +16,23 @@ def tif_pipeline_gpr(fname_in, fname_out):
     # import image_processing
 
     image = image_processing.import_tif_file(fname=fname_in)
-    print(image.shape)
 
     mean = np.empty_like(image)
 
-    lengthscale = 10
+    lengthscale = 5
     threshold = 0.05
 
-    for i in subdivided_array_slices(image, step_size=50):  # Subdivide image into parts; we create a model for each
+    for i in subdivided_array_slices(image, step_size=30):  # Subdivide image into parts; we create a model for each
         # part due to memory constraints.
         shape_of_image_part = tuple([j.stop - j.start for j in i])
         slices_of_considered_part = []
         for j in range(len(i)):  # For creating each model, we only consider points above threshold which are
-            # within 2 * lengthscale of the bounds of the part of the image we are generating. We assume the
+            # within 5 * lengthscale of the bounds of the part of the image we are generating. We assume the
             # contribution of points outside this area to the calculated mean is negligible.
-            lower = i[j].start - int(np.ceil(2*lengthscale))
+            lower = i[j].start - int(np.ceil(5*lengthscale))
             if lower < 0:
                 lower = 0
-            upper = i[j].stop + int(np.ceil(2*lengthscale))
+            upper = i[j].stop + int(np.ceil(5*lengthscale))
             if upper > image.shape[j]:
                 upper = image.shape[j]
 
@@ -52,10 +51,6 @@ def tif_pipeline_gpr(fname_in, fname_out):
             mean_part = model.predict_f(grid_coord_list)[0].numpy().reshape(shape_of_image_part)
             mean[i] = mean_part
 
-    mean *= 100  # Scale mean to show more contrast.
-    print(mean.shape)
-    x = subdivided_array_slices(mean, (1, 5, 5))
-    for i in range(1000):
-        print(mean[x[i]])
-    tifffile.imsave(fname_out, mean)  # Save image
+    mean *= 255 / np.amax(mean)  # Scale mean to show more contrast.
+    tifffile.imsave(fname_out, mean.astype("uint8"))  # Save image
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
-from core import *
-import tensorflow as tf
+
 import example_programs
+from core import *
 
 example_programs.tif_pipeline_gpr(
     "D:/test_funcMaps_fig2D_Batch_correlation_2regs_VARoption/save_fish10_fig2D_Batch_correlation_2regs_VARoption.mat_anat.tif",
