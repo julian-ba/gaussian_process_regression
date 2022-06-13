3de0ca934ab242d7ba1bd88276126f318e1b345f
Julian <julianbaier2001@gmail.com>
2022-06-13 11:38:39 +0200

    updated gitignore

---
+++ user_values.py
@@ -0,0 +1,7 @@
+MEMORY_STEP_2D = (200, 200)  # How many pixels in (x, y) should be evaluated at once in a 2-d image.
+MEMORY_STEP_3D = (1, 50, 50)
+GRID_STEP_3D = (8., 0.406, 0.406)  # Step of testing data in microns, in order (delta z, delta x, delta y)
+GRID_STEP_2D = (0.406, 0.406)
+THRESHOLD = 0.05
+
+DATA_DIRECTORY = "C:/Users/jfb20/Desktop/test_funcMaps_fig2D_Batch_correlation_2regs_VARoption/func_tif/"
